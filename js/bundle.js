ce72c200831928bafdfc4b1abdda02a5ff0d13f9
SergeevA <workhorns@gmail.com>
2023-03-31 13:42:40 +0700

    Start project

---
+++ js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+console.log();
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
