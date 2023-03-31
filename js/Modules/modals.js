b1da7e4a023bad2f9344d02c7e71651cd31ddc02
SergeevA <workhorns@gmail.com>
2023-03-31 15:38:51 +0700

    Открытие модального окна по ссылке.

--- js/Modules/modals.js
+++ js/Modules/modals.js
@@ -1,46 +1,104 @@
-//Переменная модального окна с функцией колбека.
+// Переменная модального окна с функцией колбека.
+
+//Версия 2.0
 const modals = () => {
     //Принимает 3 параметра
-    function bindModal(trigger, modal, close) {
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const   trigger = document.querySelectorAll(triggerSelector),
+                modal = document.querySelector(modalSelector),
+                close = document.querySelector(closeSelector);
+
+
         //Парам. Тригера нажатия
-        trigger.addEventListener('click', (event) => {
-            if(event.target) {
-                event.preventDefault();
-            }
-            modal.style.display = 'block';
-            document.body.style.overflow ='hidden';
-            // console.log(callEngineerBtn)
+        trigger.forEach(item => {
+            item.addEventListener('click', (event) => {
+                if(event.target) {
+                    event.preventDefault();
+                }
+                modal.style.display = 'block';
+                document.body.style.overflow ='hidden'; //Старый функционал
+            });
         });
         
         //Парам. Нажатия на крестик
         close.addEventListener('click', () => {
                 modal.style.display = 'none';
-                document.body.style.overflow ='';
-                // console.log(modalEngineerClose)
+                document.body.style.overflow =''; //Старый функционал
         });
 
         //Парам. Нажатия на свободную область.
         modal.addEventListener('click', (event) => {
             if(event.target === modal){
                 modal.style.display = 'none';
-                document.body.style.overflow ='';
-                // console.log(modalEngineer)
+                document.body.style.overflow =''; //Старый функционал
             }
         });
     }
-    //Создание переменных для взаимо действия.
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-        modalEngineer = document.querySelector('.popup_engineer'),
-        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow ='hidden'; //Старый функционал
+
+        }, time)
+    }
 
     //Вызов функции и передача переменных.
-    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
-
-    //Проверка видимости нажатия
-    // console.log(callEngineerBtn)
-    // console.log(modalEngineer)
-    // console.log(modalEngineerClose)
-
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 60000)
 };
 
 export default modals;
+
+//Версия 1.0
+// const modals = () => {
+//     //Принимает 3 параметра
+//     function bindModal(trigger, modal, close) {
+
+//         //Парам. Тригера нажатия
+//         trigger.addEventListener('click', (event) => {
+//                 if(event.target) {
+//                     event.preventDefault();
+//                 }
+//                 modal.style.display = 'block';
+//                 document.body.style.overflow ='hidden'; //Старый функционал
+//                 // console.log(callEngineerBtn)
+//                 // document.body.classList.add('modal-open') //Функционал через bootstrap
+//             });
+        
+        
+//         //Парам. Нажатия на крестик
+//         close.addEventListener('click', () => {
+//                 modal.style.display = 'none';
+//                 document.body.style.overflow =''; //Старый функционал
+//                 // console.log(modalEngineerClose)
+//                 // document.body.classList.remove('modal-open') //Функционал через bootstrap
+//         });
+
+//         //Парам. Нажатия на свободную область.
+//         modal.addEventListener('click', (event) => {
+//             if(event.target === modal){
+//                 modal.style.display = 'none';
+//                 document.body.style.overflow =''; //Старый функционал
+//                 // console.log(modalEngineer)
+//                 // document.body.classList.remove('modal-open') //Функционал через bootstrap
+//             }
+//         });
+//     }
+//     //Создание переменных для взаимо действия. // Старый вариант
+//     const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+//         modalEngineer = document.querySelector('.popup_engineer'),
+//         modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+//     // Вызов функции и передача переменных.
+//     bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+
+//     //Проверка видимости нажатия
+//     // console.log(callEngineerBtn)
+//     // console.log(modalEngineer)
+//     // console.log(modalEngineerClose)
+
+// };
+
+// export default modals;
--- js/bundle.js
+++ js/bundle.js
@@ -12,51 +12,106 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-//Переменная модального окна с функцией колбека.
+// Переменная модального окна с функцией колбека.
+
+//Версия 2.0
 const modals = () => {
   //Принимает 3 параметра
-  function bindModal(trigger, modal, close) {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
     //Парам. Тригера нажатия
-    trigger.addEventListener('click', event => {
-      if (event.target) {
-        event.preventDefault();
-      }
-      modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
-      // console.log(callEngineerBtn)
+    trigger.forEach(item => {
+      item.addEventListener('click', event => {
+        if (event.target) {
+          event.preventDefault();
+        }
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden'; //Старый функционал
+      });
     });
 
     //Парам. Нажатия на крестик
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      document.body.style.overflow = '';
-      // console.log(modalEngineerClose)
+      document.body.style.overflow = ''; //Старый функционал
     });
 
     //Парам. Нажатия на свободную область.
     modal.addEventListener('click', event => {
       if (event.target === modal) {
         modal.style.display = 'none';
-        document.body.style.overflow = '';
-        // console.log(modalEngineer)
+        document.body.style.overflow = ''; //Старый функционал
       }
     });
   }
-  //Создание переменных для взаимо действия.
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-    modalEngineer = document.querySelector('.popup_engineer'),
-    modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden'; //Старый функционал
+    }, time);
+  }
 
   //Вызов функции и передача переменных.
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
-
-  //Проверка видимости нажатия
-  // console.log(callEngineerBtn)
-  // console.log(modalEngineer)
-  // console.log(modalEngineerClose)
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalByTime('.popup', 60000);
 };
-
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
+
+//Версия 1.0
+// const modals = () => {
+//     //Принимает 3 параметра
+//     function bindModal(trigger, modal, close) {
+
+//         //Парам. Тригера нажатия
+//         trigger.addEventListener('click', (event) => {
+//                 if(event.target) {
+//                     event.preventDefault();
+//                 }
+//                 modal.style.display = 'block';
+//                 document.body.style.overflow ='hidden'; //Старый функционал
+//                 // console.log(callEngineerBtn)
+//                 // document.body.classList.add('modal-open') //Функционал через bootstrap
+//             });
+
+//         //Парам. Нажатия на крестик
+//         close.addEventListener('click', () => {
+//                 modal.style.display = 'none';
+//                 document.body.style.overflow =''; //Старый функционал
+//                 // console.log(modalEngineerClose)
+//                 // document.body.classList.remove('modal-open') //Функционал через bootstrap
+//         });
+
+//         //Парам. Нажатия на свободную область.
+//         modal.addEventListener('click', (event) => {
+//             if(event.target === modal){
+//                 modal.style.display = 'none';
+//                 document.body.style.overflow =''; //Старый функционал
+//                 // console.log(modalEngineer)
+//                 // document.body.classList.remove('modal-open') //Функционал через bootstrap
+//             }
+//         });
+//     }
+//     //Создание переменных для взаимо действия. // Старый вариант
+//     const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+//         modalEngineer = document.querySelector('.popup_engineer'),
+//         modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+//     // Вызов функции и передача переменных.
+//     bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+
+//     //Проверка видимости нажатия
+//     // console.log(callEngineerBtn)
+//     // console.log(modalEngineer)
+//     // console.log(modalEngineerClose)
+
+// };
+
+// export default modals;
 
 /***/ }),
 
